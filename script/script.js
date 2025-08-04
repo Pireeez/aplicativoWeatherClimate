9002f5ae9307f54d615fe84b9a5b832ed5345438
Pireeez <gustavo.pires@lab2dev.com>
2025-08-04 12:02:13 -0300

    update na estilização de exibição das informações sobre o climaalteração de nome das variáveis

--- script/script.js
+++ script/script.js
@@ -1,20 +1,22 @@
-
 const btnHeader = document.querySelector('#button-header')
     .addEventListener('click', () => {
         let inputHeader = document.querySelector('#input-header').value;
-        buscarApi(inputHeader)
+        fetchCityData(inputHeader)
     })
 
-const buscarApi = async(inputValue) => {
+//requisição API geocodificação usando nome da cidade e envia resultado para 
+const fetchCityData = async(inputValue) => {
     try {
         const data = await fetch(`https://geocoding-api.open-meteo.com/v1/search?name=${inputValue}`).then(data => data.json());
-        criaListaCidades(data)
+
+        createCityList(data)
     } catch (erro) {
         alert("Alguam coisa deu errado",erro)
     }
 }
 
-const criaListaCidades = (data) => {
+//cria uma lista de cidade para cada resultado retornado pela api
+const createCityList = (data) => {
     const gridWeek = document.querySelector('.grid-week');
     gridWeek.innerHTML = '';
     const cityInfo = data.results.map(data => `${data.name} - ${data.admin1}, ${data.country}`);
@@ -23,33 +25,122 @@ const criaListaCidades = (data) => {
             div.className = 'box-week';
             div.textContent = cityInfo[i];
             div.addEventListener('click', () => {
-                getCityInfo(data.results[i]);
+                updateCityInfo(data.results[i]);
             })
             gridWeek.appendChild(div);
         }
 }
 
-const getCityInfo = (info) => {
+//atualiza as informações na tela (nome,pais,lat,lon) e chama api para buscar o clima dessa localização
+const updateCityInfo = (info) => {
     const lat = info.latitude
     const lon = info.longitude
     document.querySelector('#location-city-main').textContent = info.name;;
     document.querySelector('#location-coutry-main').textContent = `${info.admin1} - ${info.country}`;
-    document.querySelector('#latitude-info').textContent = `Latitude: ${lat}`;
-    document.querySelector('#longitude-info').textContent = `longitude: ${lon}`;
+    document.querySelector('#latitude-info').textContent = lat;
+    document.querySelector('#longitude-info').textContent = lon;
     
-    getClimaAPI(lat,lon);
+    fetchWeatherData(lat,lon);
 }
 
-const getClimaAPI = async(lat,lon) => {
+//Faz requisição API de clima usando latitude e longitude e envia os dados para atualização de tela do clima
+const fetchWeatherData = async(lat,lon) => {
     try {
         const data = await fetch(`https://api.open-meteo.com/v1/forecast?latitude=${lat}&longitude=${lon}&current_weather=true`).then(data => data.json())
-        console.log(data);
+        updateWeatherInfo(data);
     } catch (error) {
-        console.log('Deu algum erro',error);
+        console.log('Erro a procurar dados da API:',error);
     }
 }
 
+//mostra na tela info de temperatura, vento, descrição do clima e altera o visual conforme for dia e noite
+const updateWeatherInfo = (data) => {
+    const temp = data.current_weather.temperature;//temperatura
+    const day = data.current_weather.is_day;// se é noite: 0 se é dia: 1
+    const windDirection = data.current_weather.winddirection;//direção do vento
+    const weatherCode = data.current_weather.weathercode;//código do clima
+    const windspeed = data.current_weather.windspeed;//velocidade do vento
+    const unitTemperatura = data.current_weather_units.temperature;//unidade de temperatura
+    const unitWindDirection = data.current_weather_units.winddirection;//unidade da direção do vento
+    const unitWindSpeed = data.current_weather_units.windspeed;//unidade da velocidade do vento
 
+    document.querySelector('#temperature').textContent = temp+unitTemperatura;
+    document.querySelector('#weather').textContent = getWeatherDescription(weatherCode)
+    document.querySelector('#wind').textContent = windspeed+unitWindSpeed
+    document.querySelector('#windDirect').textContent = windDirection+unitWindDirection
+    if(day === 1){
+        //dia
+        document.querySelector('body').style = 'background: linear-gradient(300deg, var(--background-gradiente1-day), var(--background-gradiente2-day), var(--background-gradiente3-day)); background-size: 600% 600%; animation: gradientShift 3s ease infinite;';
+        document.querySelector('.grid-main').style = 'color: var(--color-text-day)';
+        document.querySelector('#weather-image').style = 'content: url(/img/sol.png);'
+    }
+    if(day === 0){
+        //noite
+        document.querySelector('body').style = 'background: linear-gradient(300deg, var(--background-gradiente1-night), var(--background-gradiente2-night), var(--background-gradiente3-night)); background-size: 600% 600%; animation: gradientShift 3s ease infinite;';
+        document.querySelector('.grid-main').style = 'color: var(--color-text-night)';
+        document.querySelector('#weather-image').style = 'content: url(/img/limpoNoite.png);'
+    }
+}
+
+//recebe código de condição climática da api e retorna descrição
+const getWeatherDescription = (codigo) => { 
+    switch(codigo){
+        case 0: return "Céu limpo";
+        case 1: return "Principalmente limpo";
+        case 2: return "Parcialmente nublado";
+        case 3: return "Nublado";
+        case 45: return "Nevoeiro";
+        case 48: return "Nevoeiro com geada";
+        case 51: return "Chuvisco leve";
+        case 53: return "Chuvisco moderado";
+        case 55: return "Chuvisco forte";
+        case 61: return "Chuva leve";
+        case 63: return "Chuva moderada";
+        case 65: return "Chuva forte";
+        case 66: return "Chuva de granizo leve";
+        case 67: return "Chuva de granizo forte";
+        case 71: return "Nevasca leve";
+        case 73: return "Nevasca moderada";
+        case 75: return "Nevasca forte";
+        case 77: return "Neve granular";
+        case 80: return "Pancadas de chuva leves";
+        case 81: return "Pancadas de chuva moderadas";
+        case 82: return "Pancadas de chuva fortes";
+        case 85: return "Pancadas de neve leves";
+        case 86: return "Pancadas de neve fortes";
+        case 95: return "Trovoada";
+        case 96: return "Trovoada com granizo leve";
+        case 99: return "Trovoada com granizo forte";
+        default: return "Código desconhecido";
+    }
+}
+
+// 0   Céu limpo  
+// 1   Principalmente limpo  
+// 2   Parcialmente nublado  
+// 3   Nublado  
+// 45  Nevoeiro  
+// 48  Nevoeiro com geada  
+// 51  Chuvisco leve  
+// 53  Chuvisco moderado  
+// 55  Chuvisco forte  
+// 61  Chuva leve  
+// 63  Chuva moderada  
+// 65  Chuva forte  
+// 66  Chuva de granizo leve  
+// 67  Chuva de granizo forte  
+// 71  Nevasca leve  
+// 73  Nevasca moderada  
+// 75  Nevasca forte  
+// 77  Neve granular  
+// 80  Pancadas de chuva leves  
+// 81  Pancadas de chuva moderadas  
+// 82  Pancadas de chuva fortes  
+// 85  Pancadas de neve leves  
+// 86  Pancadas de neve fortes  
+// 95  Trovoada  
+// 96  Trovoada com granizo leve  
+// 99  Trovoada com granizo forte  
 
 // Para cada cidade, devem ser exibidos os seguintes dados:
 
